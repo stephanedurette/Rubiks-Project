8a9611156a037dde8f79b4c3fca406f36b800825
stephanedurette <stephanedurette@rogers.com>
2026-04-01 22:47:55 -0400

    no message

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,27 +1,55 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     private void Start()
     {
+        var c = new Cube();
+        //TestSequence(c, new Cube.Move[] { c.F, c.R, c.D}); //doesn't work
+        //TestSequence(c, new Cube.Move[] { c.R, c.D }); //doesn't work
+
+        //TestSequence(c, new Cube.Move[] { c.R, c.U }); //doesn't work
+        //TestSequence(c, new Cube.Move[] { c.F, c.U }); //works
+        //TestSequence(c, new Cube.Move[] { c.F, c.R }); //works
+        //TestSequence(c, new Cube.Move[] { c.D }); //works
+        //TestSequence(c, new Cube.Move[] { c.R }); //works
+
+        //TestSequence(c, new Cube.Move[] { c.F, c.L, c.D, c.L_, c.U, c.B, c.D_}); //doesn't work
+    }
+
+    private void TestSequence(Cube c, Cube.Move[] moves)
+    {
+        string[] states = new string[moves.Length];
+
+        string initialState = c.ToString();
+
+        for (int i = 0; i < moves.Length; i++)
+        {
+            moves[i].Execute();
+            states[i] = c.ToString();
+        }
+
+        for (int i = moves.Length - 1; i >= 0; i--)
+        {
+            Debug.Log(c.ToString() == states[i]);
+            moves[i].Reverse();
+        }
+
+        Debug.Log(c.ToString() == initialState);
+    }
+
+    private void TestAllMoves()
+    {
         Cube c = new();
 
-        Debug.Log(c);
+        string startingState = c.ToString();
 
-        var moveList = c.GetRandomMoveList(15);
-        
-        foreach (var move in moveList)
+        foreach (var move in c.Moves)
         {
             move.Execute();
+            move.Reverse();
+
+            Debug.Log(c.ToString() == startingState);
         }
-
-        Debug.Log(c);
-
-        for (int i = moveList.Length - 1; i >= 0; i--) { 
-            moveList[i].Reverse();
-        }
-
-        Debug.Log(c);
     }
 }
